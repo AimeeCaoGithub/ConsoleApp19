d70315dac211d1cda7b97085c0c37416d4b32714
Aimee Cao <v-liucao@microsoft.com>
2021-01-28 07:42:42 +0000

    testtext

--- WebApplication1/Class.cs
+++ WebApplication1/Class.cs
@@ -9,5 +9,6 @@ namespace WebApplication1
     {
         //fsdf
         //changes
+        //changes
     }
 }
---
+++ WebApplication1/Class1 - Copy.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class Class1
+    {
+    }
+}
